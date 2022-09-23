13ad8dc2604e03e6d1cf1c12de689b5a2fcad822
sheik0101 <sshasan@deloitte.com>
2022-09-24 01:16:33 +0530

    Added success message

--- src/components/Description.vue
+++ src/components/Description.vue
@@ -1,14 +1,36 @@
 <template>
   <div id="description-block" class="description">
     <div class="heading">
-      <span class="description-text">Description</span>
+      <span class="description-text">{{ title }}</span>
       <a @click="closePopup" class="close-icn">X</a>
     </div>
-    <textarea
-      placeholder="Type your description here"
-      class="text-area"
-    ></textarea>
-    <button class="go-btn" @click="submit">
+    <div class="text-block">
+      <textarea
+        placeholder="Type your description here"
+        class="text-area"
+      ></textarea>
+      <button class="go-btn" @click="submit">
+        <svg
+          version="1.1"
+          id="Layer_1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          x="0px"
+          y="0px"
+          viewBox="0 0 16.77 28.4"
+          style="enable-background: new 0 0 16.77 28.4"
+          xml:space="preserve"
+        >
+          <path
+            class="st0"
+            d="M3.96,1.51c-0.68-0.68-1.78-0.68-2.45,0C0.84,2.19,0.83,3.27,1.47,3.95l0.04,0.04l10.34,10.45L1.51,24.9
+	c-0.66,0.67-0.68,1.75-0.04,2.44l0.04,0.04c0.66,0.67,1.73,0.68,2.41,0.04l0.04-0.04l11.57-11.69c0.66-0.67,0.68-1.75,0.04-2.44
+	l-0.04-0.04L3.96,1.51z"
+          />
+        </svg>
+      </button>
+    </div>
+    <div style="display: none" class="success-icn">
       <svg
         version="1.1"
         id="Layer_1"
@@ -16,18 +38,17 @@
         xmlns:xlink="http://www.w3.org/1999/xlink"
         x="0px"
         y="0px"
-        viewBox="0 0 16.77 28.4"
-        style="enable-background: new 0 0 16.77 28.4"
+        viewBox="0 0 92.2 67.5"
+        style="enable-background: new 0 0 92.2 67.5"
         xml:space="preserve"
       >
         <path
           class="st0"
-          d="M3.96,1.51c-0.68-0.68-1.78-0.68-2.45,0C0.84,2.19,0.83,3.27,1.47,3.95l0.04,0.04l10.34,10.45L1.51,24.9
-	c-0.66,0.67-0.68,1.75-0.04,2.44l0.04,0.04c0.66,0.67,1.73,0.68,2.41,0.04l0.04-0.04l11.57-11.69c0.66-0.67,0.68-1.75,0.04-2.44
-	l-0.04-0.04L3.96,1.51z"
+          d="M28.18,66.86c-0.75,0-1.46-0.3-1.99-0.82L1.82,41.66c-1.1-1.1-1.1-2.88,0-3.98c1.1-1.1,2.88-1.1,3.98,0
+	l22.38,22.38L86.42,1.82c1.1-1.1,2.88-1.1,3.98,0c1.1,1.1,1.1,2.88,0,3.98L30.17,66.03C29.65,66.56,28.93,66.86,28.18,66.86z"
         />
       </svg>
-    </button>
+    </div>
   </div>
 </template>
       
@@ -36,17 +57,23 @@ export default {
   name: "UploadImage",
   data() {
     return {
+      title: "Description",
       submitted: false,
     };
   },
   methods: {
     closePopup() {
-      document.getElementById("map-frame").style.height="90vh";
+      document.getElementById("map-frame").style.height = "90vh";
       document.getElementById("description-block").classList.remove("show");
       document.getElementById("description-block").classList.add("hide");
+      this.title = "Description";
+      document.getElementsByClassName("text-block")[0].style.display = "block";
+      document.getElementsByClassName("success-icn")[0].style.display = "none";
     },
     submit() {
-      console.log("submitted");
+      this.title = "Success";
+      document.getElementsByClassName("text-block")[0].style.display = "none";
+      document.getElementsByClassName("success-icn")[0].style.display = "flex";
     },
   },
 };
@@ -122,6 +149,21 @@ export default {
     left: 50%;
     svg {
       width: 12px;
+    }
+  }
+  .success-icn {
+    fill: #fff;
+    background: #73ddc1;
+    border-radius: 50%;
+    width: 50px;
+    height: 50px;
+    padding: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    svg {
+      width: 30px;
+      height: 30px;
     }
   }
 }
