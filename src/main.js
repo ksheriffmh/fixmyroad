3611631db3c478e6e9ba873bba4332c61be95521
sheik0101 <sshasan@deloitte.com>
2022-09-24 02:07:23 +0530

    Added firebase

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
 import { createApp } from 'vue'
 import Home from './Home.vue'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCkUavfmA-f2U314DxtyvI6XEI_1Q1wbDI",
+  authDomain: "fixmyroad-1db86.firebaseapp.com",
+  projectId: "fixmyroad-1db86",
+  storageBucket: "fixmyroad-1db86.appspot.com",
+  messagingSenderId: "282885597864",
+  appId: "1:282885597864:web:89d1182f603a3a7351f09d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 createApp(Home).mount('#app')
